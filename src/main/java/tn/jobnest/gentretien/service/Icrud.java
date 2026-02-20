cafacbebf55493fc6e476ac3e0375586cac68ac0
GHaziz04 <Medaziz.ghribi@esprit.tn>
2026-02-20 09:12:38 +0100

    Delete src directory

--- src/main/java/tn/jobnest/gentretien/service/Icrud.java
+++
@@ -1,9 +0,0 @@
-package tn.jobnest.gentretien.service;
-import java.sql.SQLException;
-import java.util.List;
-public interface Icrud<T> {
-    void ajouter(T t) throws SQLException;
-    void update(T t) throws SQLException;
-    void delete(int id) throws SQLException;
-    List<T> afficher() throws SQLException;
-}
